e3093aa2d13f5959982434de1c5bf5382f615f30
keyxcode <keyxcode@gmail.com>
2023-09-19 15:45:53 +0700

    Add ArrayQueue boilerplates

---
+++ 3-ArrayQueue/AQProgram.java
@@ -0,0 +1,7 @@
+public class AQProgram {
+    public static void main(String[] args) { 
+        ArrayQueue<String> myQueue = new ArrayQueue<>();
+
+        System.out.println(myQueue);
+    }    
+}
